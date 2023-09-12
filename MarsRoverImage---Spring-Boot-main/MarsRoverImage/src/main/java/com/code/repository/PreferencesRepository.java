f9fe7a687dd79694b9e8ffcd63b3ca600e70b0c7
amyasar <132398533+amyasar@users.noreply.github.com>
2023-09-12 19:15:48 -0400

    Delete MarsRoverImage---Spring-Boot-main/MarsRoverImage directory

--- MarsRoverImage---Spring-Boot-main/MarsRoverImage/src/main/java/com/code/repository/PreferencesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.code.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.code.dto.HomeDto;
-
-public interface PreferencesRepository extends JpaRepository<HomeDto, Long> {
-  HomeDto findByUserId(Long userId);
-}
